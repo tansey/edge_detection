400d4429a633fc5d15abd012db3a6ab8bab3524e
Wesley Tansey <wes.tansey@gmail.com>
2011-11-06 21:52:46 -0600

    Fixed Gaussian filter. Added car example.

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ import itertools
 def gaussian(x, y, sigma):
 	return (1 / (2*math.pi*sigma**2))*math.exp(-(x**2+y**2)/(2*sigma**2))
 
-im = Image.open("stapler.png")
+name = "stapler"
+
+im = Image.open(name + ".png")
 print im.format, im.size, im.mode
 bw = im.convert("L")
-bw.save("bw_stapler.png", "PNG")
+bw.save(name + "_bw.png", "PNG")
 bw_pix = bw.load()
 width = bw.size[0]
 height = bw.size[1]
@@ -17,18 +19,28 @@ sigma = 1
 convolution = [ [0 for col in range(bw.size[1])] for row in range(bw.size[0])]
 # Loop over every pixel in the image and apply the Gaussian filter
 for (x,y) in itertools.product(range(bw.size[0]), range(bw.size[1])):
+	gauss_totals = 0
 	# For each neighboring pixel
 	for(u, v) in itertools.product(range(-3*sigma + x,3*sigma+1+x),range(-3*sigma + y,3*sigma+1+y)):
 		if(u < 0 or v < 0 or u >= width or v >= height):
 			continue
-		convolution[x][y] += (bw_pix[u,v] / 255) * gaussian(x-u,y-v,sigma)
-	#print x, y, convolution[x][y]
+		gauss_totals += gaussian(x-u,y-v,sigma)
+		convolution[x][y] += (bw_pix[u,v]) * gaussian(x-u,y-v,sigma)
+	
+	convolution[x][y] /= gauss_totals
 
 	# sanity check
-	if convolution[x][y] > 1:
-		raise Exception("Problem!")
+	#if convolution[x][y] > 1:
+	#	raise Exception("Problem!")
 
-gradient_threshold = 0.1
+smoothed = Image.new(bw.mode, bw.size)
+smoothed_pix = smoothed.load()
+for x, xval in enumerate(convolution):
+	for y, yval in enumerate(convolution[x]):
+		smoothed_pix[x,y] = int(convolution[x][y])
+smoothed.save(name + "_smoothed.png")
+
+gradient_threshold = 20
 
 neighbor_offsets = [(-1,-1), (-1,0), (-1, 1), (0,1), (0, -1), (1,0), (1,1), (1,-1)]
 
@@ -70,5 +82,5 @@ for (x,y) in itertools.product(range(bw.size[0]), range(bw.size[1])):
 	if is_max:
 		edges_pix[x,y] = 0
 
-edges.save("edges.png", "PNG")
+edges.save(name + "_edges.png", "PNG")
 
