e2899f501b1981c5aeb9c319ffb4f03e0ecf2ae5
Wesley Tansey <wes.tansey@gmail.com>
2011-11-07 14:01:44 -0600

    Fixed the edge detection algorithm and added support for orientation detection.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import itertools
 def gaussian(x, y, sigma):
 	return (1 / (2*math.pi*sigma**2))*math.exp(-(x**2+y**2)/(2*sigma**2))
 
-name = "stapler"
+name = "car"
 
 im = Image.open(name + ".png")
 print im.format, im.size, im.mode
@@ -40,47 +40,50 @@ for x, xval in enumerate(convolution):
 		smoothed_pix[x,y] = int(convolution[x][y])
 smoothed.save(name + "_smoothed.png")
 
-gradient_threshold = 20
+gradient_threshold = 10
 
-neighbor_offsets = [(-1,-1), (-1,0), (-1, 1), (0,1), (0, -1), (1,0), (1,1), (1,-1)]
+neighbor_offsets = [(-1,-1,225), (-1,0,270), (-1,1,315), (0,1,0), (0,-1,180), (1,0,90), (1,1,45), (1,-1,125)]
 
-gradients = [ [0 for col in range(bw.size[1])] for row in range(bw.size[0])]
+gradients = [ [(0,0) for col in range(bw.size[1])] for row in range(bw.size[0])]
 
 # Loop over every pixel in the image and calculate the largest gradient
 for (x,y) in itertools.product(range(bw.size[0]), range(bw.size[1])):
 	pixel = convolution[x][y]
 	# For each immediately neighboring pixel
-	for (uidx, vidx) in neighbor_offsets:
+	for (uidx, vidx, angle) in neighbor_offsets:
 		u = x + uidx
 		v = y + vidx
 		if u < 0 or y < 0 or u >= width or v >= height:
 			continue
 		gradient = abs(convolution[u][v] - pixel)
-		if gradient > gradients[x][y]:
-			gradients[x][y] = gradient
+		if gradient > gradients[x][y][0]:
+			gradients[x][y] = (gradient, angle)
 
 # Draw the edges
 edges = bw.point(lambda i: 255)
 edges_pix = edges.load()
 
+results = [ [(False,0) for col in range(bw.size[1])] for row in range(bw.size[0])]
+
 # Loop over every pixel in the image and determine if it's an edge pixel
 for (x,y) in itertools.product(range(bw.size[0]), range(bw.size[1])):
 	gradient = gradients[x][y]
 	# the gradient must be greater than some threshold set by the user
-	if gradient < gradient_threshold:
+	if gradient[0] < gradient_threshold:
 		continue
 	is_max = True
 	# determine if the gradient is a local maximum
-	for (uidx, vidx) in neighbor_offsets:
+	for (uidx, vidx, angle) in neighbor_offsets:
 		u = x + uidx
 		v = y + vidx
 		if u < 0 or y < 0 or u >= width or v >= height:
 			continue
-		if gradients[u][v] > pixel:
+		if gradients[u][v][0] > gradient[0]:
 			is_max = False
 			break
 	if is_max:
 		edges_pix[x,y] = 0
+		results[x][y] = (True,angle)
 
 edges.save(name + "_edges.png", "PNG")
 
